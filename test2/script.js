e6dcdd9795c8b42451c3fd6cf0fcf21f3f7719c8
sweedishhfish04 <godziwa4@yahoo.com>
2018-08-25 12:12:16 -0500

    "removed repeated code"

--- test2/script.js
+++ test2/script.js
@@ -1,19 +1,19 @@
 // Get the modal
 var modal = document.getElementById('modal');
 
-// Get the button that opens the modal
+// Get the button that opens the signup modal
 var btn = document.getElementById("signup");
 
-// Get the <span> element that closes the modal
+// Get the <span> element that closes the signup modal
 var span = document.getElementsByClassName("close")[0];
 
 // Get the modal
 var modalLogin = document.getElementById('modalLogin');
 
-// Get the button that opens the modal
+// Get the button that opens the login modal
 var btnLogin = document.getElementById("login");
 
-// Get the <span> element that closes the modal
+// Get the <span> element that closes the login modal
 var spanLogin = document.getElementsByClassName("closeLogin")[0];
 
 // When the user clicks the button, open the modal 
@@ -40,11 +40,4 @@ btnLogin.onclick = function () {
 // When the user clicks on <span> (x), close the modal
 spanLogin.onclick = function () {
     modalLogin.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modalLogin) {
-        modalLogin.style.display = "none";
-    }
 }
