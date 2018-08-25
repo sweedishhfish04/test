e69287e2513e5e3dccf04678241bac7e287c1a9c
sweedishhfish04 <godziwa4@yahoo.com>
2018-08-25 12:24:13 -0500

    added else if to window.onclick

--- test2/script.js
+++ test2/script.js
@@ -1,4 +1,4 @@
-// Get the modal
+// Get the signup modal
 var modal = document.getElementById('modal');
 
 // Get the button that opens the signup modal
@@ -7,7 +7,7 @@ var btn = document.getElementById("signup");
 // Get the <span> element that closes the signup modal
 var span = document.getElementsByClassName("close")[0];
 
-// Get the modal
+// Get the login modal
 var modalLogin = document.getElementById('modalLogin');
 
 // Get the button that opens the login modal
@@ -28,16 +28,17 @@ span.onclick = function () {
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+        if (event.target == modal) {
+            modal.style.display = "none";
+        } else if (event.target == modalLogin) {
+            modalLogin.style.display = "none";
+        }
     }
-}
-
-// When the user clicks the button, open the modal 
-btnLogin.onclick = function () {
-    modalLogin.style.display = "block";
-}
-// When the user clicks on <span> (x), close the modal
-spanLogin.onclick = function () {
-    modalLogin.style.display = "none";
-}
+        // When the user clicks the button, open the modal 
+        btnLogin.onclick = function () {
+            modalLogin.style.display = "block";
+        }
+        // When the user clicks on <span> (x), close the modal
+        spanLogin.onclick = function () {
+            modalLogin.style.display = "none";
+        }
